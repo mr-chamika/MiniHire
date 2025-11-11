a25147005ad4d8fd647319af30e6bc30c16b86d8
mr-chamika <chamikauni2001@gmail.com>
2025-11-11 09:58:51 +0530

    layout updated

--- app/(auth)/(pages)/layout.tsx
+++ app/(auth)/(pages)/layout.tsx
@@ -8,7 +8,9 @@ export default function AuthLayout({ children }: { children: React.ReactNode }) 
     return (
         <div>
             <AuthTopbar />
-            {children}
+            <div className="pt-20">
+                {children}
+            </div>
         </div>
     )
 
